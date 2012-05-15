f54b7d72be1d4d680b95849beaed67d0fb586489
Lukhnos Liu <lukhnos@cs.stanford.edu>
2012-05-15 01:01:38 -0700

    Add a script to test the rename example.

---
+++ tests/SimpleRename/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export FROM_CLASS_NAME="class SampleNameSpace::Foo"
+export TO_CLASS_NAME="Foobar"
+cd ../../
+make
+cd -
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:STRING=ON .
+../../refactorial Foo.cpp
