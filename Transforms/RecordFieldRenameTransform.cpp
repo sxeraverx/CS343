0f15f1bb46978eebdc03c13d90b63f5b6f471c82
Lukhnos Liu <lukhnos@cs.stanford.edu>
2012-06-07 00:47:33 -0700

    Regex-based rename features in place; rename fields; no Stmt yet.

--- Transforms/RecordFieldRenameTransform.cpp
+++ Transforms/RecordFieldRenameTransform.cpp
@@ -3,64 +3,55 @@
 //
 
 #include "RenameTransforms.h"
-#include "pcrecpp.h"
 
 using namespace clang;
 
 class RecordFieldRenameTransform : public RenameTransform {
 public:
   virtual void HandleTranslationUnit(ASTContext &) override;
+  void collectAndRenameFieldDecl(DeclContext *DC);
   
 protected:
-  typedef std::pair<pcrecpp::RE, std::string> REStringPair;
-  std::vector<REStringPair> renameList;
 };
 
 REGISTER_TRANSFORM(RecordFieldRenameTransform);
 
 void RecordFieldRenameTransform::HandleTranslationUnit(ASTContext &C)
 {
-  auto S = TransformRegistry::get().config["RecordFieldRename"];
-  if (!S.IsMap()) {
-    llvm::errs() << "Error: Cannot find config entry \"RecordFieldRename\""
-      " or entry is not a map\n";
+  auto I = loadConfig("RecordFieldRename", "Fields");
+  if (!I) {
     return;
   }
   
-  auto IG = S["Ignore"];
+  auto TUD = C.getTranslationUnitDecl();  
+  collectAndRenameFieldDecl(TUD);
+}
+
+void RecordFieldRenameTransform::collectAndRenameFieldDecl(DeclContext *DC)
+{
+  pushIndent();
   
-  if (IG && !IG.IsSequence()) {
-    llvm::errs() << "Error: Config key \"Ignore\" must be a sequence\n";
-    return;
-  }
-  
-  for (auto I = IG.begin(), E = IG.end(); I != E; ++I) {
-    if (I->IsScalar()) {
-      llvm::errs() << "Ignoring: " << I->as<std::string>() << "\n";
-    }
-  }
-  
-  auto RN = S["Fields"];
-  if (!RN.IsSequence()) {
-    llvm::errs() << "No fields specified or is not a sequence\n";
-    return;
-  }
-  
-  for (auto I = RN.begin(), E = RN.end(); I != E; ++I) {
-    if (!I->IsMap()) {
-      llvm::errs() << "Error: \"Fields\" contains non-map items\n";
-      break;
+  for(auto I = DC->decls_begin(), E = DC->decls_end(); I != E; ++I) {
+    auto L = (*I)->getLocation();
+    if (shouldIgnore(L)) {
+      continue;
     }
     
-    for (auto MI = I->begin(), ME = I->end(); MI != ME; ++MI) {
-      auto F = MI->first.as<std::string>();
-      auto T = MI->second.as<std::string>();      
-      pcrecpp::RE re(F);
-      renameList.push_back(REStringPair(re, T));
+    if (auto D = dyn_cast<FieldDecl>(*I)) {
+      llvm::errs() << indent() << "Field: " << D->getQualifiedNameAsString() << "\n";
       
-      llvm::errs() << "renames: " << F << " -> " << T << "\n";
+      std::string newName;
+      if (nameMatches(D, newName)) {
+        llvm::errs() << indent() << "Rename to: " << newName << "\n";
+        renameLocation(D->getLocation(), newName);
+      }
+    }
+    
+    // descend into the next level (namespace, etc.)    
+    if (auto innerDC = dyn_cast<DeclContext>(*I)) {
+      collectAndRenameFieldDecl(innerDC);
     }
   }
-  
-  
+  popIndent();  
 }
+
--- Transforms/RenameTransforms.h
+++ Transforms/RenameTransforms.h
@@ -2,6 +2,7 @@
 #define RENAME_TRANSFORMS_H
 
 #include "Transforms.h"
+#include <pcrecpp.h>
 #include <clang/Lex/Preprocessor.h>
 
 class RenameTransform : public Transform {
@@ -9,6 +10,110 @@ public:
   RenameTransform() : indentLevel(0) {}
 protected:
   // utility functions shared by all rename transforms
+  
+  bool loadConfig(const std::string& transformName,                        
+                  const std::string& renameKeyName,
+                  const std::string& ignoreKeyName = "Ignore") {
+    auto S = TransformRegistry::get().config[transformName];
+    if (!S.IsMap()) {
+      llvm::errs() << "Error: Cannot find config entry \"" << transformName
+                   << "\" or entry is not a map\n";
+      return false;
+    }
+
+    auto IG = S[ignoreKeyName];
+
+    if (IG && !IG.IsSequence()) {
+      llvm::errs() << "Error: Config key \"" << ignoreKeyName
+                   << "\" must be a sequence\n";
+      return false;
+    }
+
+    for (auto I = IG.begin(), E = IG.end(); I != E; ++I) {
+      if (I->IsScalar()) {
+        auto P = I->as<std::string>();
+        ignoreList.push_back(pcrecpp::RE(P));
+        llvm::errs() << "Ignoring: " << P << "\n";
+      }
+    }
+
+    auto RN = S[renameKeyName];
+    if (!RN.IsSequence()) {
+      llvm::errs() << "\"" << renameKeyName << "\" is not specified or is"
+                   << " not a sequence\n";
+      return false;
+    }
+
+    for (auto I = RN.begin(), E = RN.end(); I != E; ++I) {
+      if (!I->IsMap()) {
+        llvm::errs() << "Error: \"" << renameKeyName
+                     << "\" contains non-map items\n";
+        return false;
+      }
+
+      for (auto MI = I->begin(), ME = I->end(); MI != ME; ++MI) {
+        auto F = MI->first.as<std::string>();
+        auto T = MI->second.as<std::string>();      
+        pcrecpp::RE re(F);
+        renameList.push_back(REStringPair(re, T));
+
+        llvm::errs() << "renames: " << F << " -> " << T << "\n";
+      }
+    }
+    
+    return true;
+  }
+  
+  bool shouldIgnore(clang::SourceLocation L) {
+    if (!L.isValid()) {
+      return true;
+    }
+
+    clang::SourceManager &SM = sema->getSourceManager();
+    clang::FullSourceLoc FSL(L, SM);
+    const clang::FileEntry *FE = SM.getFileEntryForID(FSL.getFileID());
+    if (!FE) {
+      return true;
+    }
+
+    auto FN = FE->getName();
+
+    for (auto I = ignoreList.begin(), E = ignoreList.end(); I != E; ++I) {
+      if (I->FullMatch(FN)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+  
+  bool nameMatches(const clang::NamedDecl *D, std::string &outNewName,
+                   bool checkOnly = false) {
+    auto I = nameMap.find(D);
+    if (I != nameMap.end()) {
+      outNewName = (*I).second;
+      return true;
+    }
+    
+    // if we only need a quick look-up of renamed Decl's
+    // (e.g. for renamed parent classes, overridden methods etc.)
+    if (checkOnly) {
+      return false;
+    }
+
+    auto QN = D->getQualifiedNameAsString();
+    for (auto I = renameList.begin(), E = renameList.end(); I != E; ++I) {
+      if (I->first.FullMatch(QN)) {
+        std::string newName;
+        I->first.Extract(I->second, QN, &newName);
+        nameMap[D] = newName;
+        outNewName = newName;
+        return true;
+      }
+    }
+  
+    return false;
+  }
   
   void renameLocation(clang::SourceLocation L, std::string& N) {
     if (L.isValid()) {
@@ -54,6 +159,12 @@ protected:
 private:
   int indentLevel;
   std::string indentString;
+
+  std::vector<pcrecpp::RE> ignoreList;
+  typedef std::pair<pcrecpp::RE, std::string> REStringPair;
+  std::vector<REStringPair> renameList;
+
+  std::map<const clang::Decl *, std::string> nameMap;  
 };
 
 #endif
